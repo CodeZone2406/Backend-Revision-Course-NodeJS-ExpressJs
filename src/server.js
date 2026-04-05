9050f09deef24a3c57f9b3cc3cfd47a708e5539b
Arbaaz Ansari <ansarbaaz@hotmail.com>
2026-04-05 19:26:31 +0300

    Add health check endpoint to server

--- src/server.js
+++ src/server.js
@@ -14,6 +14,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  res.send("Server is running!");
+});
+
 app.use("/movies", movieRoutes);
 app.use("/auth", authRoutes);
 app.use("/watchList", watchlistRoute)
